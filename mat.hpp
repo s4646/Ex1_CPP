7ca8c38a3071fbedce0cfab3e3355be65d5e7237
Sahar Tuvyahu <sahar45456@gmail.com>
2022-03-04 18:40:37 +0200

    Added headers

---
+++ mat.hpp
@@ -0,0 +1,15 @@
+#ifndef MAT_INCLUDED
+#define MAT_INCLUDED
+
+#include <string>
+using namespace std;
+
+namespace ariel
+{ 
+    string convertToString(char* a, int size);  
+    string evenRow(int rowIndex, int columns, char a, char b);
+    string oddRow(int rowIndex, int columns, char a, char b);
+    string mat(int x, int y, char a, char b);
+}
+
+#endif // MAT_INCLUDED
